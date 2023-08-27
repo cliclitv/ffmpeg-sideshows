64bfc0c9646c9712977c55a6fa629f9354786eaa
unknown <1533540012@qq.com>
2023-08-27 15:56:50 +0800

    日常提交

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from "path"
+
+export default defineConfig({
+  plugins: [vue()],
+  
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src")
+    },
+  },
+  build:{
+    outDir:'./docs'
+  },
+  server: {
+    // host: '0.0.0.0', //只能使用localhost
+    //处理ffmpeg的SharedArrayBuffer is not defined
+    headers: {
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Opener-Policy": "same-origin",
+    },
+  },
+})
